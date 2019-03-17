f9c83717cdcca1d799141fd83eeba51b93fb340c
Jackson Prince <jdprince555@gmail.com>
2019-03-17 20:42:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,93 @@
 var cart = [];
 
-function getCart() {
+function addToCart(item) {
+  
+////Create a function that produces a random number between 1 - 100 for each new item added to the cart.
+
+function randomGenerator() {
+  return Math.floor(Math.random()*100);
+}
+
+////Create a new object for each new item added to the cart, consisting of itemName and itemPrice (using randomGenerator).
+
+  var addedItem = {
+      itemName: `${item}`,
+      itemPrice: randomGenerator()
+  };
+  
+////Add the new item and return the successful message.
+
+  cart.push(addedItem);
+  return `${item} has been added to your cart.`;
+}
+
+////
+////Next function, viewCart()
+////
+////
+  
+function viewCart() {
+  
+  var cartList = "";
+  //Print empty cart message, 
+  
+  if (cart.length === 0) {return "Your shopping cart is empty."}
+  
+  //If just one item, then return the interpolated sentence: 
+  
+  else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  
+  //Otherwise create concatenation string via for loop.
+  
+  else {
+      for (var i = 0; i < cart.length - 1; i++) {
+      cartList = cartList + `${cart[i].itemName} at $${cart[i].itemPrice}, `
+            }
+          cartList = cartList + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+          return `In your cart, you have ${cartList}`
+        }
+
+  }
+  
+////
+////Next function, total()
+////
+////
+  
+  
+function total() {
+  var totalValue = 0;
+  for (var i = 0; i < cart.length; i++) {
+      totalValue = totalValue + cart[i].itemPrice
+  }
+  return totalValue;
+}
+
+////
+////Next function, removeFromCart()
+////
+////
+
+function removeFromCart(item) {
+  
+  //interesting strategy: function findIt() {
+    //while (cartola[i].itemName != "Pibb") {
+    //i++;
+    //}
+    //console.log(i);
+//}
+}
+
+  function getCart() {
  return cart;
 }
 
 function setCart(c) {
   cart = c;
-  return cart;
-}
-
-function addToCart(item) {
- // write your code here
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
+  return cart; 
+  
 }
 
 function placeOrder(cardNumber) {
